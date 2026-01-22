91712db76172a5889b68b917e3755d350f418412
Fluffy Riot <me@fluffyriot.com>
2026-01-22 17:16:20 +0000

    - added temporary logo - new default font for better readability - buttons simplification

--- internal/api/handlers/settings.go
+++ internal/api/handlers/settings.go
@@ -50,6 +50,7 @@ func (h *Handler) SyncSettingsHandler(c *gin.Context) {
 	c.HTML(http.StatusOK, "sync-settings.html", gin.H{
 		"sync_period":        user.SyncPeriod,
 		"enabled_on_startup": user.EnabledOnStartup,
+		"worker_running":     h.Worker.IsActive(),
 		"app_version":        config.AppVersion,
 	})
 }
